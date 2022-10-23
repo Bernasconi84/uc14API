d74b286d1e07a22a4c32beac846c657789ae4214
Bernasconi84 <bernasconi84@outlook.com>
2022-10-23 02:03:12 -0300

    primeiro commit

---
+++ Chapter/Chapter/Contexts/ChapterContexts.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Chapter.Models;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContexts:DbContext
+    {
+        public ChapterContexts()
+        {
+
+        }
+        public ChapterContexts(DbContextOptions<ChapterContexts>options) :base(options)
+        {
+
+        }
+
+        //vamos utilizar esse método para configurar o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //cada provedor tem sua sintaxe para especificação
+                 optionsBuilder.UseSqlServer("Data Source = DESKTOP-6NSHU44\\SQLEXPRESS ; initial catalog = Chapter ; Integrated Security = true");
+            }
+        }
+
+        //DbSet representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e exclusão
+        public DbSet<Models.Livro> Livros { get; set; }
+
+
+    }
+}
---
+++ Chapter/Chapter/Repositories/LivroRepository.cs
@@ -0,0 +1,19 @@
+﻿using Chapter.Contexts;
+using Chapter.Models;
+
+namespace Chapter.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContexts _contexts;
+
+        public LivroRepository(ChapterContexts contexts)
+        {
+            _contexts = contexts;
+        }
+        public List<Livro> Listar()
+        {
+            return _contexts.Livros.ToList();
+        }
+    }
+}

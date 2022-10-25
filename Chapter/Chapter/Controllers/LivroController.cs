72425a236aaf33f32a71edff2652bc88c463f9b5
Bernasconi84 <bernasconi84@outlook.com>
2022-10-24 22:25:31 -0300

    Segundo commit Metodos GET POST PUT DELETE

--- Chapter/Chapter/Controllers/LivroController.cs
+++ Chapter/Chapter/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using Chapter.Repositories;
+﻿using Chapter.Models;
+using Chapter.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,20 +15,96 @@ namespace Chapter.Controllers
         public LivroController(LivroRepository livroRepository)
         {
             _livroRepository = livroRepository;
-        }   
+        }
 
         [HttpGet]
         public IActionResult Listar()
         {
-            try 
+            try
             {
                 return Ok(_livroRepository.Listar());
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 throw new Exception(e.Message);
             }
-           
+
         }
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livroBuscado = _livroRepository.BuscarPorId(id);
+
+                if (livroBuscado == null)
+                {
+                    return NotFound();
+                }
+                return Ok(livroBuscado);
+
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+
+                return Ok("Livro removido");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        
+        public IActionResult Atualizar(int id, Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+         
+
     }
 }
--- Chapter/Chapter/Repositories/LivroRepository.cs
+++ Chapter/Chapter/Repositories/LivroRepository.cs
@@ -15,5 +15,40 @@ namespace Chapter.Repositories
         {
             return _contexts.Livros.ToList();
         }
+        public Livro BuscarPorId(int id)
+        {
+            return _contexts.Livros.Find(id);
+        }
+        public void Cadastrar(Livro livro)
+        {
+            _contexts.Livros.Add(livro);
+
+            _contexts.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Livro livro = _contexts.Livros.Find(id);
+
+            _contexts.Livros.Remove(livro);
+
+            _contexts.SaveChanges();
+        }
+
+        public void Atualizar(int id, Livro livro)
+        {
+            Livro livroBuscado = _contexts.Livros.Find(id); 
+
+            if (livroBuscado != null)
+            {
+                livroBuscado.Titulo = livro.Titulo;
+                livroBuscado.QuantidadePaginas = livro.QuantidadePaginas;
+                livroBuscado.Disponivel = livro.Disponivel;
+            }
+
+            _contexts.Update(livroBuscado);
+
+            _contexts.SaveChanges();
+        }
     }
 }
